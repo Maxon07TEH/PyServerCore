ffcfc418bae70375a298e65d443ad4bbca14c80e
Maxon07TEH <maksbutenko78@gmail.com>
2024-07-14 17:35:39 +0300

    добавление файла виа загрузка
    сделал пример сайта и иконки+подгрузка сайта по требованию

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #на библиотеке pySocket, настройки в консоли, сервак
 import socket
-import string
+#import string
 
 #сеттинг#
-HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
 maxdatavolume = input('pacSize?- ')
 localhostip = ("'" + input('ip?- ') + "'")
 localhostport = input('port?- ')
@@ -11,13 +10,13 @@ msgencode = "utf-8"
 defmsg = "сервер работает, комп горяч"
 LOGfile_patch: str = ("'" + input("logPatch?- ") + "'")
 
-if maxdatavolume == "def" or "default":
+if maxdatavolume == "def" or "default" or "":
     maxdatavolume = 2048
-if localhostip == "def" or "default" or "'def'" or "'default'":
+if localhostip == "def" or "default" or "'def'" or "'default'" or "":
     localhostip = "127.0.0.1"
-if localhostport == "def" or "default":
+if localhostport == "def" or "default" or "":
     localhostport = 2000
-if LOGfile_patch == "def" or "default" or "'def'" or "'default'":
+if LOGfile_patch == "def" or "default" or "'def'" or "'default'" or "":
     LOGfile_patch = "connectLog.txt"
 def WriteLOG(LOGcontent):
     LOGfile = open(LOGfile_patch, 'r+')
@@ -25,17 +24,28 @@ def WriteLOG(LOGcontent):
     LOGfile.close()
 
 def StartServer():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((localhostip, localhostport))
-    server.listen(8)
-    print("Server started")
-    user, adres = server.accept()
-    data = user.recv(maxdatavolume).decode(msgencode)
-    print(data)
-    user.send(HDRS.encode(msgencode) + defmsg.encode(msgencode))
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((localhostip, localhostport))
+        server.listen(8)
+        while True:
+            print("Server started")
+            user, adres = server.accept()
+            data = user.recv(maxdatavolume).decode(msgencode)
+            print(data)
+            user.send(load_page_from_get_request(data))
+            user.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print("оффнулся")
 
-def LoadPage(request_data):
-    path = request_data.split("")[1]
+def load_page_from_get_request(request_data):
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    path = request_data.split(" ")[1]
+    response = ""
+    with open("htmlview"+path, "rb") as file:
+        response = file.read()
+        return HDRS.encode(msgencode) + response
 
-while True:
-    print("while")
+if __name__ == "__main__":
+    StartServer()
